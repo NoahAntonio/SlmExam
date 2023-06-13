19c849b9a3d851a671bb9a527f1fd84834e20179
Noah Seyhan <wi22b128@technikum-wien.at>
2023-06-13 08:11:57 +0200

    Initial commit

---
+++ src/test/java/at/technikum/slmexam/SlmExamApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.slmexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
